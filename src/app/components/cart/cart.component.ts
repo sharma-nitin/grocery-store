74c1a62ff1ff4a5bbd39161c7698abeebe7364af
sharma-nitin <sharmanitin1492@gmail.com>
2021-01-22 19:48:43 +0530

    cart responsiveness

--- src/app/components/cart/cart.component.ts
+++ src/app/components/cart/cart.component.ts
@@ -8,12 +8,19 @@ import { ProductService } from 'src/app/services/product.service';
 })
 export class CartComponent implements OnInit {
   itemsInCart = localStorage.getItem('products') ? JSON.parse(localStorage.getItem('products')) : [];
+  cartValue = this.itemsInCart.reduce(function(prev, cur) {
+    return prev + (cur.price * cur.quantity);
+  }, 0);
+  
   constructor(public Productservice:ProductService) { }
 
   ngOnInit() {
     this.Productservice.setFooterStatus(false);
     this.Productservice.cartProducts$.subscribe((res:any)=>{
       this.itemsInCart = res;
+      this.cartValue = this.itemsInCart.reduce(function(prev, cur) {
+        return prev + (cur.price * cur.quantity);
+      }, 0);
     })
   }
 
